233bb6584bbe5a4961bfdd7376c5dd8afc7558b3
evgeniums <evgeniums@dracosha.com>
2024-03-19 15:48:55 +0300

    Fill declaration of ConfigTree

--- base/include/hatn/base/configtree.h
+++ base/include/hatn/base/configtree.h
@@ -12,7 +12,7 @@
 */
 /** @file base/configtree.h
   *
-  * Contains declarations of configuration tree types and functions.
+  * Contains declarations of configuration tree class.
   *
   */
 
@@ -21,6 +21,7 @@
 #ifndef HATNCONFIGTREE_H
 #define HATNCONFIGTREE_H
 
+#include <hatn/common/error.h>
 
 #include <hatn/base/base.h>
 #include <hatn/base/baseerror.h>
@@ -29,11 +30,59 @@
 
 HATN_BASE_NAMESPACE_BEGIN
 
+ /**
+ * @brief The ConfigTree class represents a tre of values that can be accessed with literal nested path.
+ */
 class HATN_BASE_EXPORT ConfigTree : public ConfigTreeValue
 {
     public:
 
-        using ConfigTreeValue::ConfigTreeValue;
+        /**
+         * @brief Constructor.
+         * @param pathSeparator Separator between sections of nested path. Default is ".".
+         */
+        ConfigTree(const std::string& pathSeparator="."):m_pathSeparator(pathSeparator)
+        {}
+
+        ConfigTree(ConfigTree&&)=default;
+        ConfigTree& operator =(ConfigTree&&)=default;
+        ~ConfigTree()=default;
+
+        ConfigTree(const ConfigTree&)=delete;
+        ConfigTree& operator =(const ConfigTree&)=delete;
+
+        using ConfigTreeValue::set;
+        using ConfigTreeValue::toArray;
+        using ConfigTreeValue::toMap;
+        using ConfigTreeValue::reset;
+
+        template <typename T>
+        Result<ConfigTree&> set(const common::lib::string_view& path, T&& value, bool autoCreatePath=true) noexcept;
+
+        template <typename T>
+        ConfigTree& set(const common::lib::string_view& path, T&& value, Error &ec, bool autoCreatePath=true) noexcept;
+
+        template <typename T>
+        ConfigTree& setEx(const common::lib::string_view& path, T&& value, bool autoCreatePath=true);
+
+        Result<const ConfigTree&> get(const common::lib::string_view& path) const noexcept;
+        const ConfigTree& get(const common::lib::string_view& path, Error &ec) const noexcept;
+        const ConfigTree& getEx(const common::lib::string_view& path) const;
+
+        Result<ConfigTree&> get(const common::lib::string_view& path, bool autoCreatePath=true) noexcept;
+        ConfigTree& get(const common::lib::string_view& path, Error &ec, bool autoCreatePath=true) noexcept;
+        ConfigTree& getEx(const common::lib::string_view& path, bool autoCreatePath=true);
+
+        template <typename T>
+        auto toArray(const common::lib::string_view& path) -> decltype(auto);
+
+        config_tree::MapT& toMap(const common::lib::string_view& path);
+
+        void reset(const common::lib::string_view& path) noexcept;
+
+    private:
+
+        std::string m_pathSeparator;
 };
 
 
