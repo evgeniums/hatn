b7adacc23b8adab43195d4bc989cda57aa4bd50f
evgeniums <evgeniums@dracosha.com>
2024-06-25 18:52:49 +0300

    Fix cmake warning

--- logcontext/test/test.cmake
+++ logcontext/test/test.cmake
@@ -11,6 +11,6 @@ ADD_HATN_CTESTS(logcontext ${TEST_SOURCES} ${TEST_HEADERS})
 FUNCTION(TestLogcontext)
     COPY_LIBRARY_HERE(hatncommon${LIB_POSTFIX} ../common/)
     COPY_LIBRARY_HERE(hatnlogcontext${LIB_POSTFIX} ../logcontext/)
-ENDFUNCTION(TestBase)
+ENDFUNCTION(TestLogcontext)
 
 ADD_CUSTOM_TARGET(logcontexttest-src SOURCES ${TEST_HEADERS} ${TEST_SOURCES} ${SOURCES})
