ef7ae72c27fc11216470ee666544584748521fac
evgeniums <evgeniums@dracosha.com>
2024-11-12 13:06:43 +0300

    Add ByteString class

--- common/include/hatn/common/pmr/string.h
+++ common/include/hatn/common/pmr/string.h
@@ -69,80 +69,114 @@ class ByteAllocator
 constexpr size_t StringPreallocatedSize=64;
 
 template <size_t PreallocatedSize=StringPreallocatedSize, typename AllocatorT=ByteAllocator<>>
-struct StringT : public fmt::basic_memory_buffer<char,PreallocatedSize,AllocatorT>
+class StringT : public fmt::basic_memory_buffer<char,PreallocatedSize,AllocatorT>
 {
-    using BaseT=fmt::basic_memory_buffer<char,PreallocatedSize,AllocatorT>;
+    public:
 
-    StringT(
-        const AllocatorT& alloc=AllocatorT{}
-        )
-        : BaseT(alloc)
-    {}
+        using BaseT=fmt::basic_memory_buffer<char,PreallocatedSize,AllocatorT>;
+        using AllocT=AllocatorT;
+        using SelfT=StringT<PreallocatedSize,AllocatorT>;
 
-    template <typename ContainerT>
-    StringT(const ContainerT& container,
-            const AllocatorT& alloc=AllocatorT{}
-            )
-        : BaseT(alloc)
-    {
-        this->append(container);
-    }
+        StringT(
+                const AllocatorT& alloc=AllocatorT{}
+            ) : BaseT(alloc)
+        {}
 
-    StringT(const char* str,
-            const AllocatorT& alloc=AllocatorT{}
-            )
-        : StringT(
-            lib::string_view{str},
-            alloc)
-    {}
-
-    ~StringT()=default;
-    StringT(StringT&& other)=default;
-    StringT& operator=(StringT&& other)=default;
-
-    StringT(const StringT& other) : StringT(other.get_allocator())
-    {
-        this->append(other);
-    }
-
-    StringT& operator=(const StringT& other)
-    {
-        if (&other!=this)
+        template <typename ContainerT>
+        StringT(const ContainerT& container,
+                const AllocatorT& alloc=AllocatorT{}
+                )
+            : BaseT(alloc)
         {
-            this->clear();
+            this->append(container);
+        }
+
+        StringT(const char* str,
+                const AllocatorT& alloc=AllocatorT{}
+                )
+            : StringT(
+                lib::string_view{str},
+                alloc)
+        {}
+
+        ~StringT()=default;
+        StringT(StringT&& other)=default;
+        StringT& operator=(StringT&& other)=default;
+
+        StringT(const StringT& other) : StringT(other.get_allocator())
+        {
             this->append(other);
         }
-        return *this;
-    }
 
-    lib::string_view toStringView() const noexcept
-    {
-        return lib::string_view{this->data(),this->size()};
-    }
+        StringT& operator=(const StringT& other)
+        {
+            if (&other!=this)
+            {
+                this->clear();
+                this->append(other);
+            }
+            return *this;
+        }
 
-    std::string toString() const
-    {
-        return std::string{this->data(),this->size()};
-    }
+        lib::string_view toStringView() const noexcept
+        {
+            return lib::string_view{this->data(),this->size()};
+        }
 
-    operator lib::string_view() const noexcept
-    {
-        return toStringView();
-    }
+        std::string toString() const
+        {
+            return std::string{this->data(),this->size()};
+        }
 
-    operator std::string() const
-    {
-        return toString();
-    }
+        operator lib::string_view() const noexcept
+        {
+            return toStringView();
+        }
 
-    template <typename T>
-    bool operator<(const T& other) const noexcept
-    {
-        return toStringView()<lib::string_view{other};
-    }
-    //! @todo Add <=> operator
+        operator std::string() const
+        {
+            return toString();
+        }
+
+        template <typename T>
+        bool operator<(const T& other) const noexcept
+        {
+            return toStringView()<lib::string_view{other};
+        }
+        //! @todo Add <=> operator
 };
-using String=StringT<>;
+
+template <size_t PreallocatedSize=StringPreallocatedSize>
+class ByteString : public StringT<PreallocatedSize>
+{
+    public:
+
+        using BaseT=StringT<PreallocatedSize>;
+        using AllocT=ByteAllocator<>;
+
+        ByteString(
+                pmr::memory_resource* resource
+            ) : BaseT(AllocT{resource})
+        {}
+
+        ByteString(
+                pmr::AllocatorFactory* factory
+            ) : BaseT(AllocT{factory->dataMemoryResource()})
+        {}
+
+        template <typename ...Args>
+        ByteString(Args... args) : BaseT(std::forward<Args>(args)...)
+        {}
+
+        template <typename T>
+        bool operator<(const T& other) const noexcept
+        {
+            return this->toStringView()<lib::string_view{other};
+        }
+        //! @todo Add <=> operator
+};
+
+using String=ByteString<>;
 
 } // namespace pmr
 
