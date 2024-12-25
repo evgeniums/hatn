e4aa2e6b71b8e1a8ba512d9809934aecc6d03166
evgeniums <evgeniums@dracosha.com>
2024-12-25 15:03:53 +0300

    Add hatn.mq library

---
+++ mq/include/hatn/mq/mq.h
@@ -0,0 +1,46 @@
+/*
+    Copyright (c) 2020 - current, Evgeny Sidorov (decfile.com), All rights reserved.
+    
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+      
+*/
+
+/****************************************************************************/
+/*
+    
+*/
+/** @file mq/mq.h
+  *
+  *  Hatn MQ Library implements a Message Queue.
+  *
+  */
+
+/****************************************************************************/
+
+#ifndef HATNMQ_H
+#define HATNMQ_H
+
+#include <hatn/mq/config.h>
+
+// define export symbols for windows platform
+#ifdef WIN32
+#    ifndef HATN_MQ_EXPORT
+#        ifdef BUILD_HATN_MQ
+#            define HATN_MQ_EXPORT __declspec(dllexport)
+#        else
+#            define HATN_MQ_EXPORT __declspec(dllimport)
+#        endif
+#    endif
+#else
+#    define HATN_MQ_EXPORT
+#endif
+
+#define HATN_MQ_NAMESPACE_BEGIN namespace hatn { namespace mq {
+#define HATN_MQ_NAMESPACE_END }}
+
+#define HATN_MQ_NAMESPACE hatn::mq
+#define HATN_MQ_NS mq
+#define HATN_MQ_USING using namespace hatn::mq;
+
+#endif // HATNMQ_H
