7fc7edbbb5a4bed91e147e469de7e00bf3392fae
evgeniums <evgeniums@dracosha.com>
2024-12-25 15:03:53 +0300

    Add hatn.mq library

--- cmake/hatn/Libs.cmake
+++ cmake/hatn/Libs.cmake
@@ -8,4 +8,5 @@ SET (HATN_MODULES
 	base
 	crypt
 	db
+	mq
 )
---
+++ mq/test/testmq.cpp
@@ -0,0 +1,31 @@
+/*
+    Copyright (c) 2020 - current, Evgeny Sidorov (decfile.com), All rights reserved.
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+*/
+
+/****************************************************************************/
+/*
+
+*/
+/** @file mq/test/testmq.cpp
+  */
+
+/****************************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include "hatn_test_config.h"
+
+#include <hatn/mq/mq.h>
+
+BOOST_AUTO_TEST_SUITE(TestMq)
+
+BOOST_AUTO_TEST_CASE(Basic)
+{
+    BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
