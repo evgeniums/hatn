b9971d9015b1b77e24917b1fd784fed63d8dd0e5
evgeniums <evgeniums@dracosha.com>
2025-02-28 16:52:37 +0300

    DbObject inherits from UnitWrapper

--- db/include/hatn/db/object.h
+++ db/include/hatn/db/object.h
@@ -26,6 +26,7 @@
 #include <hatn/common/makeshared.h>
 #include <hatn/common/allocatoronstack.h>
 
+#include <hatn/dataunit/unitwrapper.h>
 #include <hatn/dataunit/syntax.h>
 
 #include <hatn/db/db.h>
@@ -87,7 +88,7 @@ constexpr const size_t ReservedTopicLength=ObjectId::Length;
 
 using TopicContainer=HATN_COMMON_NAMESPACE::StringOnStackT<ReservedTopicLength>;
 
-class DbObject
+class DbObject : public du::UnitWrapper
 {
     public:
 
@@ -96,216 +97,60 @@ class DbObject
         template <typename T>
         DbObject(HATN_COMMON_NAMESPACE::SharedPtr<T> sharedUnit,
                     lib::string_view topic=lib::string_view{}
-                    ) :
-            m_shared(sharedUnit.template staticCast<HATN_DATAUNIT_NAMESPACE::Unit>()),
-            m_topic(topic)
+                    ) : UnitWrapper(std::move(sharedUnit)),
+                        m_topic(topic)
         {}
 
         DbObject(HATN_COMMON_NAMESPACE::SharedPtr<HATN_DATAUNIT_NAMESPACE::Unit> sharedUnit,
                     lib::string_view topic=lib::string_view{}
                     ) :
-            m_shared(std::move(sharedUnit)),
+            UnitWrapper(std::move(sharedUnit)),
             m_topic(topic)
         {}
-
-        template <typename T>
-        T* unit()
-        {
-            static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::unit_tag,T>,"T must be of Unit type");
-
-            static T sample;
-            return sample.castToUnit(m_shared.get());
-        }
-
-        template <typename T>
-        const T* unit() const
-        {
-            static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::unit_tag,T>,"T must be of Unit type");
-
-            static T sample;
-            return sample.castToUnit(m_shared.get());
-        }
-
-        template <typename T>
-        T* managedUnit()
-        {
-            static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::managed_unit_tag,T>,"T must be of ManagedUnit type");
-
-            static T sample;
-            return sample.castToManagedUnit(m_shared.get());
-        }
-
-        template <typename T>
-        T* managedUnit() const
-        {
-            static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::managed_unit_tag,T>,"T must be of ManagedUnit type");
-
-            static T sample;
-            return sample.castToManagedUnit(m_shared.get());
-        }
 
         lib::string_view topic() const noexcept
         {
             return m_topic;
         }
 
-        bool isNull() const noexcept
-        {
-            return this->m_shared.isNull();
-        }
+    private:
 
-        operator bool() const noexcept
-        {
-            return !isNull();
-        }
-
-        template <typename T>
-        T* get() const noexcept
-        {
-            auto self=const_cast<DbObject*>(this);
-            return self->template get<T>();
-        }
-
-        template <typename T>
-        T* get() noexcept
-        {            
-            if constexpr (hana::is_a<HATN_DATAUNIT_META_NAMESPACE::unit_tag,T>)
-            {
-                return unit<T>();
-            }
-            else if constexpr (hana::is_a<HATN_DATAUNIT_META_NAMESPACE::managed_unit_tag,T>)
-            {
-                return managedUnit<T>();
-            }
-            else
-            {
-                static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::managed_unit_tag,T>
-                                  ||
-                                  hana::is_a<HATN_DATAUNIT_META_NAMESPACE::unit_tag,T>
-                              ,"T must be either of Unit or of ManagedUnit type");
-                return T(nullptr);
-            }
-        }
-
-        template <typename T>
-        T* mutableValue() noexcept
-        {
-            return *get<T>();
-        }
-
-        template <typename T>
-        const T& value() const noexcept
-        {
-            return *get<T>();
-        }
-
-        template <typename T>
-        T* as() const noexcept
-        {
-            return get<T>();
-        }
-
-        template <typename T>
-        T* as() noexcept
-        {
-            return get<T>();
-        }
-
-        auto shared() const noexcept
-        {
-            return m_shared;
-        }
-
-        HATN_DATAUNIT_NAMESPACE::Unit* get() const noexcept
-        {
-            return m_shared.get();
-        }
-
-    protected:
-
-        HATN_COMMON_NAMESPACE::SharedPtr<HATN_DATAUNIT_NAMESPACE::Unit> m_shared;
         TopicContainer m_topic;
 };
 
 template <typename T>
-class DbObjectT : public DbObject
+class DbObjectT : public du::UnitWrapperT<T>
 {
     public:
 
-        static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::managed_unit_tag,T>,"T must be of ManagedUnit type");
+        using Base=du::UnitWrapperT<T>;
 
         DbObjectT()=default;
 
         template <typename T1>
         DbObjectT(HATN_COMMON_NAMESPACE::SharedPtr<T1> sharedUnit,
-                lib::string_view topic=lib::string_view{}
-                     ) : DbObject(std::move(sharedUnit),std::move(topic))
-        {
-            static_assert(hana::is_a<HATN_DATAUNIT_META_NAMESPACE::managed_unit_tag,T>,"T1 must be of ManagedUnit type");
-        }
+                    lib::string_view topic=lib::string_view{}
+                ) : Base(std::move(sharedUnit)),
+                    m_topic(topic)
+        {}
 
         DbObjectT(HATN_COMMON_NAMESPACE::SharedPtr<HATN_DATAUNIT_NAMESPACE::Unit> sharedUnit,
                     lib::string_view topic=lib::string_view{}
-                    ) : DbObject(std::move(sharedUnit),std::move(topic))
+                ) : Base(std::move(sharedUnit)),
+                    m_topic(topic)
         {}
 
-        DbObjectT(DbObject other) : DbObject(std::move(other))
+        DbObjectT(DbObject other) : Base(std::move(other))
         {}
 
-        T* get() const noexcept
+        lib::string_view topic() const noexcept
         {
-            return this->template managedUnit<T>();
+            return m_topic;
         }
 
-        T* get() noexcept
-        {
-            return this->template managedUnit<T>();
-        }
+    private:
 
-        T* mutableValue() noexcept
-        {
-            return *get();
-        }
-
-        const T& value() const noexcept
-        {
-            return *get();
-        }
-
-        T* operator->() const noexcept
-        {
-            return get();
-        }
-
-        T* operator->() noexcept
-        {
-            return get();
-        }
-
-        T& operator*() const noexcept
-        {
-            return value();
-        }
-
-        T& operator*() noexcept
-        {
-            return mutableValue();
-        }
-
-        //! @note Helper in case DbObjectT is used in place of DbObject
-        template <typename T1>
-        auto* as() const noexcept
-        {
-            static_assert(std::is_same<T,T1>::value,"T1 must be the same as T");
-            return get();
-        }
-
-        template <typename T1>
-        auto* as() noexcept
-        {
-            static_assert(std::is_same<T,T1>::value,"T1 must be the same as T");
-            return get();
-        }
+        TopicContainer m_topic;
 };
 
 HATN_DB_NAMESPACE_END
