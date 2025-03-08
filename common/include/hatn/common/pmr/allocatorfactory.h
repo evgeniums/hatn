3ca63e42ab37a9a6de4f850be22c11f50e20e68b
evgeniums <evgeniums@dracosha.com>
2025-03-08 21:39:57 +0300

    Update allocatorfactory.h

--- common/include/hatn/common/pmr/allocatorfactory.h
+++ common/include/hatn/common/pmr/allocatorfactory.h
@@ -189,6 +189,11 @@ class HATN_COMMON_EXPORT AllocatorFactory final
             return vector<T>(objectAllocator<T>());
         }
 
+        auto createString() const
+        {
+            return string(dataAllocator<string>());
+        }
+
     private:
 
         common::pmr::memory_resource* m_objectResourceRef;
