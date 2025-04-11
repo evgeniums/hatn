aafe6f2a9ec98d0489e15dc8fc7f6b19e24b9acb
evgeniums <evgeniums@dracosha.com>
2025-04-11 17:18:00 +0300

    Add cmake macro HATN_INSTALL_LIB

--- cmake/hatn/Install.cmake
+++ cmake/hatn/Install.cmake
@@ -6,3 +6,39 @@ INSTALL(DIRECTORY "${HATN_SOURCE_DIR}/scripts" DESTINATION lib/hatn
 INSTALL(FILES ${HATN_SOURCE_DIR}/VERSION
     DESTINATION lib/cmake/hatn
 )
+
+MACRO(HATN_INSTALL_LIB LibName)
+
+    IF (BUILD_STATIC)
+        INSTALL(TARGETS ${LibName}
+                ARCHIVE DESTINATION lib
+        )
+    ELSE (BUILD_STATIC)
+        IF (INSTALL_DEV)
+            INSTALL(TARGETS ${LibName}
+                RUNTIME DESTINATION bin
+                LIBRARY DESTINATION lib
+                ARCHIVE DESTINATION lib
+            )
+        ELSE(INSTALL_DEV)
+            INSTALL(TARGETS ${LibName}
+                RUNTIME DESTINATION bin
+                LIBRARY DESTINATION lib
+            )
+        ENDIF(INSTALL_DEV)
+    ENDIF(BUILD_STATIC)
+
+    IF (INSTALL_DEV)
+
+        SET(Args ${ARGN})
+        LIST(LENGTH Args NumArgs)
+        IF(NumArgs GREATER 0)
+            FOREACH(ARG ${Args})
+                MESSAGE(STATUS "Install include directory ${ARG}")
+                INSTALL(DIRECTORY ${ARG} DESTINATION include)
+            ENDFOREACH()
+        ENDIF()
+
+    ENDIF(INSTALL_DEV)
+
+ENDMACRO(HATN_INSTALL_LIB)
