219aecda55e4567b281004fb423c4cf03cc3d85d
evgeniums <evgeniums@dracosha.com>
2025-05-05 21:06:19 +0300

    Fix building admin server

--- adminserver/depends.cmake
+++ adminserver/depends.cmake
@@ -1,1 +1,1 @@
-SET(HATN_MODULE_DEPS validator common base logcontext dataunit network crypt db app api clientserver serverapp adminclient PARENT_SCOPE)
+SET(HATN_MODULE_DEPS validator common base logcontext dataunit network crypt db app api clientserver serverapp utility adminclient PARENT_SCOPE)
