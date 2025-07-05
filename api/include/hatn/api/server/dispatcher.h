9ae4abf58861c368d8e3bcfaf518dc06f8d6a3e7
evgeniums <evgeniums@dracosha.com>
2025-07-05 21:37:08 +0300

    Update dispatcher.h

--- api/include/hatn/api/server/dispatcher.h
+++ api/include/hatn/api/server/dispatcher.h
@@ -26,6 +26,9 @@
 #include <hatn/common/thread.h>
 #include <hatn/common/flatmap.h>
 
+#include <hatn/logcontext/postasync.h>
+#include <hatn/logcontext/makeasynccallback.h>
+
 #include <hatn/api/api.h>
 #include <hatn/api/service.h>
 #include <hatn/api/server/env.h>
@@ -39,7 +42,13 @@ namespace server {
 template <typename RequestT=Request<>>
 using DispatchCb=RouteCb<RequestT>;
 
-template <typename Traits, typename EnvT=BasicEnv, typename RequestT=Request<EnvT>>
+struct DispatcherScopes
+{
+    constexpr static const char* dispatch="dispatch";
+    constexpr static const char* dispatchCb="dispatch::cb";
+};
+
+template <typename Traits, typename EnvT=BasicEnv, typename RequestT=Request<EnvT>, typename Scopes=DispatcherScopes>
 class Dispatcher : public common::WithTraits<Traits>,
                    public std::enable_shared_from_this<Dispatcher<Traits,EnvT,RequestT>>
 {
@@ -49,21 +58,22 @@ class Dispatcher : public common::WithTraits<Traits>,
         using Request=RequestT;
         using Self=Dispatcher<Traits,EnvT,Request>;
 
-        template <typename ...TraitsArgs>
-        Dispatcher(
-                TraitsArgs&&... traitsArgs
-            )
-            : common::WithTraits<Traits>(this,std::forward<TraitsArgs>(traitsArgs)...)
-        {}
+        using common::WithTraits<Traits>::WithTraits;
 
         void dispatch(
                 common::SharedPtr<RequestContext<Request>> reqCtx,
-                DispatchCb<Request> cb
+                DispatchCb<Request> callback
             )
         {
+            auto cb=makeAsyncCallback(std::move(callback));
             auto& req=reqCtx->template get<Request>();
-            req.thread()->execAsync(
-                [reqCtx{std::move(reqCtx)},cb{std::move(cb)},this]()
+            auto* reqPtr=&req;
+            auto self=this->shared_from_this();
+            postAsync(
+                Scopes::dispatch,
+                reqPtr->thread(),
+                std::move(reqCtx),
+                [cb{std::move(cb)},this,self=std::move(self)](auto reqCtx)
                 {
                     auto cb1=[cb{std::move(cb)}](common::SharedPtr<RequestContext<Request>> reqCtx)
                     {
@@ -73,7 +83,7 @@ class Dispatcher : public common::WithTraits<Traits>,
                             // report error that no route is found
                             req.response.setStatus(protocol::ResponseStatus::RoutingError);
                         }
-                        cb(std::move(reqCtx));
+                        cb(Scopes::dispatchCb,std::move(reqCtx));
                     };
                     this->traits().dispatch(std::move(reqCtx),std::move(cb1));
                 }
