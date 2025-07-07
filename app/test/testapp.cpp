7505e410bb7f8736e30874114f82efb42520f9bd
evgeniums <evgeniums@dracosha.com>
2025-07-07 19:26:20 +0300

    Update testapp.cpp

--- app/test/testapp.cpp
+++ app/test/testapp.cpp
@@ -17,12 +17,37 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <hatn/app/appenv.h>
+
 #include "hatn_test_config.h"
+
+HATN_USING
+HATN_APP_USING
+
+class TestContext
+{
+    public:
+};
+
+using TestEnv=common::EnvTmpl<EnvWithAppEnvT,TestContext>;
 
 BOOST_AUTO_TEST_SUITE(TestApp)
 
-BOOST_AUTO_TEST_CASE(App)
+BOOST_AUTO_TEST_CASE(AppEnv)
 {
+    TestEnv env;
+
+    auto& testCtx=env.get<TestContext>();
+    static_assert(std::is_same<TestContext,std::decay_t<decltype(testCtx)>>::value,"");
+
+    auto hasTestCtx=env.hasContext<TestContext>();
+    static_assert(decltype(hasTestCtx)::value,"");
+
+    auto& factoryCtx=env.get<AllocatorFactory>();
+    static_assert(std::is_same<AllocatorFactory,std::decay_t<decltype(factoryCtx)>>::value,"");
+
+    auto hasFactoryCtx=env.hasContext<AllocatorFactory>();
+    static_assert(decltype(hasFactoryCtx)::value,"");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
