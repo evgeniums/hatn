2d9f7fe7e6f5dbff0af155ecdeb975aa458072fe
evgeniums <evgeniums@dracosha.com>
2025-08-08 17:30:40 +0300

    Update context.h

--- logcontext/include/hatn/logcontext/context.h
+++ logcontext/include/hatn/logcontext/context.h
@@ -454,7 +454,7 @@ class ContextT : public HATN_COMMON_NAMESPACE::TaskSubcontext
                 m_parentLogCtx=nullptr;
                 return;
             }
-            m_parentLogCtx=parentCtx->template get<ContextT>();
+            m_parentLogCtx=&parentCtx->template get<ContextT>();
         }
 
         void resetParentCtx(const HATN_COMMON_NAMESPACE::SharedPtr<HATN_COMMON_NAMESPACE::TaskContext>&)
