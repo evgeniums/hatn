fdb5788a08cf1a982b2ac8808be281a4453b188a
evgeniums <evgeniums@dracosha.com>
2025-10-23 15:11:20 +0300

    Update dataunit comparing

--- dataunit/include/hatn/dataunit/compare.h
+++ dataunit/include/hatn/dataunit/compare.h
@@ -36,6 +36,10 @@ bool fieldEqual(const FieldT& field, const MsgT& l, const MsgT& r)
         }
         return false;
     }
+    if (!l)
+    {
+        return false;
+    }
 
     return l->fieldValue(field)==r->fieldValue(field);
 }
@@ -51,6 +55,10 @@ bool fieldLess(const FieldT& field, const MsgT& l, const MsgT& r)
         }
         return true;
     }
+    if (!r)
+    {
+        return false;
+    }
 
     return l->fieldValue(field)<r->fieldValue(field);
 }
@@ -64,6 +72,10 @@ bool fieldsEqual(const MsgT& l, const MsgT& r, Fields&& ...fields)
         {
             return true;
         }
+        return false;
+    }
+    if (!l)
+    {
         return false;
     }
 
@@ -92,34 +104,42 @@ bool unitsEqual(const LeftT& l, const RightT& r, ExludeFields ...excludeFields)
         }
         return false;
     }
+    if (!l)
+    {
+        return false;
+    }
 
     //! @todo Implement recursive in-depth comparing
 
-    auto fieldType=[](auto&& field)
-    {
-        using type=std::decay_t<decltype(field)>;
-        return hana::type_c<type>;
-    };
-
     auto excludeFieldsTs=hana::make_tuple(std::forward<ExludeFields>(excludeFields)...);
-    auto excludeTs=hana::transform(excludeFieldsTs,fieldType);
-
-    auto each=[&l,&r,&excludeTs,&fieldType](const auto& field)
+    auto each=[&](auto&& field)
     {
-        auto fieldTypeC=fieldType(field);
         return hana::eval_if(
-            hana::contains(excludeTs,fieldTypeC),
+            hana::contains(excludeFieldsTs,field),
             []()
             {
                 return true;
             },
             [&](auto _)
             {
-                return _(l)->fieldValue(_(field)) == _(r)->fieldValue(_(field));
+                auto&& f=_(field);
+                return _(l)->fieldValue(f) == _(r)->fieldValue(f);
             }
         );
     };
-    l.iterate(each);
+
+    return hana::fold(
+        l->keys,
+        true,
+        [each](bool state, auto&& key)
+        {
+            if (!state)
+            {
+                return state;
+            }
+            return each(key);
+        }
+    );
 }
 
 template <typename SubunitFieldT, typename LeftT, typename RightT>
@@ -133,6 +153,10 @@ bool subunitsEqual(const SubunitFieldT& subunitField, const LeftT& l, const Righ
         }
         return false;
     }
+    if (!l)
+    {
+        return false;
+    }
 
     const auto& lField=l->field(subunitField);
     const auto& rField=r->field(subunitField);
@@ -143,6 +167,10 @@ bool subunitsEqual(const SubunitFieldT& subunitField, const LeftT& l, const Righ
         {
             return true;
         }
+        return false;
+    }
+    if (!rField.isSet())
+    {
         return false;
     }
 
@@ -160,6 +188,10 @@ bool repeatedSubunitsEqual(const FieldT& field, const LeftT& l, const RightT& r)
         }
         return false;
     }
+    if (!l)
+    {
+        return false;
+    }
 
     const auto& lField=l->field(field);
     const auto& rField=r->field(field);
@@ -170,6 +202,10 @@ bool repeatedSubunitsEqual(const FieldT& field, const LeftT& l, const RightT& r)
         {
             return true;
         }
+        return false;
+    }
+    if (!rField.isSet())
+    {
         return false;
     }
 
@@ -188,6 +224,10 @@ bool repeatedSubunitsEqual(const FieldT& field, const LeftT& l, const RightT& r)
             {
                 continue;
             }
+            return false;
+        }
+        if (!rEl.isSet())
+        {
             return false;
         }
 
@@ -210,6 +250,10 @@ bool repeatedFieldsEqual(const FieldT& field, const LeftT& l, const RightT& r)
         {
             return true;
         }
+        return false;
+    }
+    if (!l)
+    {
         return false;
     }
 
@@ -235,6 +279,28 @@ bool repeatedFieldsEqual(const FieldT& field, const LeftT& l, const RightT& r)
         const auto& lEl=lField.at(i);
         const auto& rEl=rField.at(i);
         auto ok=lEl==rEl;
+        if (!ok)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template <typename LeftT, typename RightT>
+bool unitVectorsEqual(const LeftT& l, const RightT& r)
+{
+    if (l.size()!=r.size())
+    {
+        return false;
+    }
+
+    for (size_t i=0;i<l.size();i++)
+    {
+        const auto& lEl=l.at(i);
+        const auto& rEl=r.at(i);
+        auto ok=unitsEqual(lEl,rEl);
         if (!ok)
         {
             return false;
